0e60d102c91b8698bad3a84303714ce9a3d62e4e
Maxime Scharwath <maxscharwath@gmail.com>
2020-03-02 14:32:45 +0100

    Main

---
+++ Labo1.cpp
@@ -0,0 +1,28 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : Labo1
+ Fichier     : Labo 1.cpp
+ Auteur(s)   : Ganguillet, Parrino et Scharwath
+ Date        : 02.03.2020
+
+ But         : TO DO
+
+ Remarque(s) : -
+
+ Compilateur : MinGW-g++ 6.3.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    system("PAUSE");
+    // cout << "Appuyez sur une touche pour continuer... ";
+    // while(cin.get() != '\n');
+    return EXIT_SUCCESS;
+}
