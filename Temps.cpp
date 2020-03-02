bfe52c60c8a2ca0624832e28e6912254a313b045
Maxime Scharwath <maxscharwath@gmail.com>
2020-03-02 14:26:28 +0100

    prout prout

---
+++ Temps.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Maxime Scharwath on 02.03.2020.
+//
+
+#include "Temps.h"
---
+++ Temps.h
@@ -0,0 +1,14 @@
+//
+// Created by Maxime Scharwath on 02.03.2020.
+//
+
+#ifndef LABO1_TEMPS_H
+#define LABO1_TEMPS_H
+
+
+class Temps {
+
+};
+
+
+#endif //LABO1_TEMPS_H
