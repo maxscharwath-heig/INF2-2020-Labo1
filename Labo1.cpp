a1d04e0e33066b25d4d0f5a4cc706fae365944e4
soso24soso <61196412+soso24soso@users.noreply.github.com>
2020-03-04 16:15:52 +0100

    modif main tests

--- Labo1.cpp
+++ Labo1.cpp
@@ -19,11 +19,42 @@
 using namespace std;
 
 int main() {
+
     Temps t1{5,10,2},
           t2(24,40,45);
-    while(true) {
-       cout << ++t1 << endl;
-    }
+
+//    friend bool operator==(const Temps &lhs, const Temps &rhs);
+
+    //test operator==
+    cout << t1 << " == " << t2 << " : " << (t1 == t2) << endl;
+
+
+
+//    friend bool operator!=(const Temps &lhs, const Temps &rhs);
+//    friend bool operator<(const Temps &lhs, const Temps &rhs);
+//    friend bool operator>(const Temps &lhs, const Temps &rhs);
+//    friend bool operator<=(const Temps &lhs, const Temps &rhs);
+//    friend bool operator>=(const Temps &lhs, const Temps &rhs);
+//    friend Temps operator +(Temps lhs , const Temps& rhs);
+//    //surcharge du flux format (hh:mm:ss)
+//    friend std::ostream &operator<<(std::ostream &os, const Temps &temps);
+//    Temps();
+//    Temps(const time_t& heureCourante);
+//    Temps( unsigned int heures, unsigned int minutes, unsigned int secondes = 0);
+//    unsigned int getHeures() const;
+//    unsigned int getMinutes() const;
+//    unsigned int getSecondes() const;
+//    void setHeures(unsigned int heures);
+//    void setMinutes(unsigned int minutes);
+//    void setSecondes(unsigned int secondes);
+//    Temps& operator+=(const Temps& rhs);
+//    Temps& operator-=(const Temps& rhs);
+//    Temps& operator++();
+//    Temps operator++(int);
+//    Temps& operator--();
+//    Temps operator--(int);
+
+
 
     return EXIT_SUCCESS;
 }
